b3bcd02af2d44205b9132c8999c2ce92a2109e9e
David Gil Baeza <davidgil.univ@gmail.com>
2019-12-09 15:56:37 +0100

    [DGIL 09/12/2019] Logging en fichero y comunicación con decide para obtener el token del usuario

--- auxiliar.py
+++ auxiliar.py
@@ -1,7 +1,20 @@
 import sqlite3
+import requests
+import logging
+
+logging.basicConfig(filename="file.log", filemode='w', level=logging.INFO,
+                    format='[%(levelname)s] - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+
+CREATE_DB_SQL = ''' CREATE TABLE USER (
+           ID INT PRIMARY KEY NOT NULL,
+           IS_LOGIN BOOLEAN DEFAULT 0,
+           USERNAME TEXT,
+           PASSWORD TEXT,
+           TOKEN TEXT
+        ); '''
 
 def check_user(id):
-    conn = sqlite3.connect('sqlite.db')
+    conn = get_db()
     cursor = conn.execute(''' SELECT * FROM USER WHERE ID = %s; ''' % id)
     conn.commit()
     user = cursor.fetchone()
@@ -9,20 +22,70 @@ def check_user(id):
     return user
 
 def create_user(id):
-    conn = sqlite3.connect('sqlite.db')
+    conn = get_db()
     conn.execute(''' INSERT INTO USER (ID) VALUES (%s); ''' % (id,))
     conn.commit()
     conn.close()
 
 def create_db():
-    conn = sqlite3.connect('sqlite.db')
+    conn = get_db()
     conn.execute(''' DROP TABLE IF EXISTS USER; ''')
     conn.commit()
-    conn.execute(''' CREATE TABLE USER (
-           ID INT PRIMARY KEY NOT NULL,
-           USERNAME TEXT,
-           PASSWORD TEXT
-        ); ''')
+    conn.execute(CREATE_DB_SQL)
     conn.commit()
     conn.close()
-    print("Base de datos creada correctamente.")
+    logging.info("Base de datos creada correctamente.")
+
+def reset_db(bot, message):
+    conn = get_db()
+    bot.send_message(message.from_user.id, "Base de datos conectada correctamente.")
+    conn.execute(''' DROP TABLE IF EXISTS USER; ''')
+    conn.commit()
+    bot.send_message(message.from_user.id, "Base de datos eliminada correctamente.")
+    conn.execute(CREATE_DB_SQL)
+    conn.commit()
+    bot.send_message(message.from_user.id, "Base de datos creada correctamente.")
+    conn.close()
+
+def set_is_login(id):
+    conn = get_db()
+    conn.execute(''' UPDATE USER SET IS_LOGIN = TRUE, USERNAME = Null, PASSWORD = Null, TOKEN = Null WHERE ID = %s; ''' % id)
+    conn.commit()
+    conn.close()
+
+def set_is_not_login(id):
+    conn = get_db()
+    conn.execute(''' UPDATE USER SET IS_LOGIN = FALSE WHERE ID = %s; ''' % id)
+    conn.commit()
+    conn.close()
+
+def check_value(id, value):
+    conn = get_db()
+    cursor = conn.execute(''' SELECT %s FROM USER WHERE ID = %s; ''' % (value, id))
+    conn.commit()
+    v = cursor.fetchone()
+    conn.close()
+    return v[0]
+
+def save_value(id, value, column):
+    conn = get_db()
+    conn.execute(''' UPDATE USER SET %s = '%s' WHERE ID = %s; ''' % (column, value, id))
+    conn.commit()
+    conn.close()
+
+def get_save_token(id, base_url):
+    res = False
+    username = check_value(id, "USERNAME")
+    pasword = check_value(id, "PASSWORD")
+    form = {
+        "username": username,
+        "password": pasword
+    }
+    response = requests.post(url=base_url + "/gateway/authentication/login/", data=form)
+    if response.status_code is 200:
+        save_value(id, response.json()['token'], "TOKEN")
+        res = True
+    return res
+
+def get_db():
+    return sqlite3.connect('sqlite.db')
--- bot.py
+++ bot.py
@@ -1,46 +1,78 @@
 import telebot
-import sqlite3
 import json
-import requests
 import auxiliar
+import logging
+
+logging.basicConfig(filename="file.log", filemode='w', level=logging.INFO,
+                    format='[%(levelname)s] - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
 try:
     TELEGRAM_TOKEN = json.load(open('data.json'))['telegram-token']
+    logging.info("TELEGRAM_TOKEN cargado correctamente.")
     CREATE_DB_COMMAND = json.load(open('data.json'))['create-db-command']
+    logging.info("CREATE_DB_COMMAND cargado correctamente.")
     bot = telebot.TeleBot(TELEGRAM_TOKEN)
-    print("Bot @%s conectado correctamente." % str(bot.get_me().username))
+    logging.info("Bot @%s conectado correctamente." % str(bot.get_me().username))
+    BASE_URL = json.load(open('data.json'))['base-url']
     auxiliar.create_db()
-except:
-    print("Por favor, indique su TELEGRAM_TOKEN y su CREATE_DB_COMMAND.")
+except Exception as e:
+    logging.error("Ha ocurrido un problema durante la inicialización.", exc_info=True)
 
 @bot.message_handler(commands=[CREATE_DB_COMMAND])
 def reset_database(message):
-    conn = sqlite3.connect('sqlite.db')
-    bot.send_message(message.from_user.id, "Base de datos conectada correctamente.")
-    conn.execute(''' DROP TABLE IF EXISTS USER; ''')
-    conn.commit()
-    bot.send_message(message.from_user.id, "Base de datos eliminada correctamente.")
-    conn.execute(''' CREATE TABLE USER (
-       ID INT PRIMARY KEY NOT NULL,
-       USERNAME TEXT,
-       PASSWORD TEXT
-    ); ''')
-    conn.commit()
-    bot.send_message(message.from_user.id, "Base de datos creada correctamente.")
-    conn.close()
+    try:
+        auxiliar.reset_db(bot, message)
+    except Exception as e:
+        logging.error("Ha ocurrido un problema al restaurar la base de datos.", exc_info=True)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    user = auxiliar.check_user(message.from_user.id)
-    if user is None:
-        bot.send_message(message.from_user.id, "¡Bienvenido a Decide, " + str(message.from_user.first_name) + "!")
-        bot.send_message(message.from_user.id, "¡Encantado de conocerte!")
-        auxiliar.create_user(message.from_user.id)
-    else:
-        bot.send_message(message.from_user.id, "¡Hola de nuevo, " + str(message.from_user.first_name) + "!")
+    try:
+        user = auxiliar.check_user(message.from_user.id)
+        if user is None:
+            bot.send_message(message.from_user.id, "¡Bienvenido a Decide, " + str(message.from_user.first_name) + "!")
+            bot.send_message(message.from_user.id, "¡Encantado de conocerte!")
+            auxiliar.create_user(message.from_user.id)
+        else:
+            bot.send_message(message.from_user.id, "¡Hola de nuevo, " + str(message.from_user.first_name) + "!")
+    except Exception as e:
+        logging.error("Ha ocurrido un problema al enviar el mensaje de bienvenida.", exc_info=True)
 
-@bot.message_handler(commands=['test'])
-def test(message):
-    bot.reply_to(message, message.text[message.json['entities'][0]['length']:])
+@bot.message_handler(commands=['login'])
+def login(message):
+    try:
+        auxiliar.set_is_login(message.from_user.id)
+        bot.send_message(message.from_user.id, "¡Vamos a guardar tus credenciales!")
+        bot.send_message(message.from_user.id, "¿Cuál es tu usuario?")
+    except Exception as e:
+        logging.error("Ha ocurrido un problema al iniciar el proceso de login.", exc_info=True)
+
+@bot.message_handler(func=lambda m: True)
+def any_message(message):
+    user_id = message.from_user.id
+    text = str(message.text).strip()
+    if auxiliar.check_value(user_id, "IS_LOGIN") and not auxiliar.check_value(user_id, "USERNAME") \
+            and not auxiliar.check_value(user_id, "PASSWORD"):
+        try:
+            auxiliar.save_value(user_id, text, "USERNAME")
+            bot.send_message(user_id, "¿Y tu contraseña?")
+        except Exception as e:
+            logging.error("Ha ocurrido un problema al guardar el username.")
+    elif auxiliar.check_value(user_id, "IS_LOGIN") and auxiliar.check_value(user_id, "USERNAME") \
+            and not auxiliar.check_value(user_id, "PASSWORD"):
+        try:
+            auxiliar.save_value(user_id, text, "PASSWORD")
+            auxiliar.set_is_not_login(user_id)
+        except Exception as e:
+            logging.error("Ha ocurrido un problema al guardar el password.", exc_info=True)
+        try:
+             if auxiliar.get_save_token(user_id, BASE_URL):
+                 bot.send_message(message.from_user.id, "¡Perfecto!")
+                 bot.send_message(message.from_user.id, "Ya hemos guardado tus credenciales.")
+             else:
+                 bot.send_message(message.from_user.id, "Lo sentimos, los datos introducidos deben ser incorrectos.")
+                 login(message)
+        except Exception as e:
+            logging.error("Ha ocurrido un problema al obtener el token.", exc_info=True)
 
 bot.polling(none_stop=True, timeout=120)
