0039ea42fe73ff9a1fb9cdb7c35b2b78e3a3d1b6
David Gil Baeza <davidgil.univ@gmail.com>
2020-01-11 14:50:13 +0100

    feature(voting-integration): Cambios realizados para adaptar la nueva votación

--- auxiliar.py
+++ auxiliar.py
@@ -5,7 +5,6 @@ import json
 from Crypto.Random import random
 
 
-
 logging.basicConfig(filename="file.log", filemode='w', level=logging.INFO,
                     format='[%(levelname)s] - %(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 
@@ -21,7 +20,9 @@ CREATE_STATUS_SQL = ''' CREATE TABLE STATUS (
             CHAT_ID INT NOT NULL,
             IS_LOGIN BOOLEAN DEFAULT 0,
             IS_VOTING BOOLEAN DEFAULT 0,
-            IS_SENDING BOOLEAN DEFAULT 0,
+            IS_SENDING_1 BOOLEAN DEFAULT 0,
+            IS_SENDING_2 BOOLEAN DEFAULT 0,
+            PRESIDENTE INT,
             FOREIGN KEY(CHAT_ID) REFERENCES USER(CHAT_ID) ON DELETE CASCADE
 ); '''
 
@@ -37,12 +38,21 @@ CREATE_VOTING_SQL = ''' CREATE TABLE VOTING (
         FOREIGN KEY(CHAT_ID) REFERENCES USER(CHAT_ID) ON DELETE CASCADE
 ); '''
 
-CREATE_OPTION_SQL = ''' CREATE TABLE OPTION (
+CREATE_CANDIDATURE_SQL = ''' CREATE TABLE CANDIDATURE (
         ID INTEGER PRIMARY KEY AUTOINCREMENT,
         VOTING_ID INT NOT NULL,
         NUMBER INT NOT NULL,
         TEXT TEXT NOT NULL,
         FOREIGN KEY(VOTING_ID) REFERENCES VOTING(VOT_ID) ON DELETE CASCADE
+); '''
+
+CREATE_CANDIDATE_SQL = ''' CREATE TABLE CANDIDATE (
+        ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        CANDIDATURE_ID INT NOT NULL,
+        NUMBER INT NOT NULL,
+        TEXT TEXT NOT NULL,
+        TYPE INTEGER DEFAULT 1,
+        FOREIGN KEY(CANDIDATURE_ID) REFERENCES CANDIDATURE(ID) ON DELETE CASCADE
 ); '''
 
 def check_user(id):
@@ -65,12 +75,14 @@ def create_db():
     conn.execute(''' DROP TABLE IF EXISTS USER; ''')
     conn.execute(''' DROP TABLE IF EXISTS STATUS; ''')
     conn.execute(''' DROP TABLE IF EXISTS VOTING; ''')
-    conn.execute(''' DROP TABLE IF EXISTS OPTION; ''')
+    conn.execute(''' DROP TABLE IF EXISTS CANDIDATURE; ''')
+    conn.execute(''' DROP TABLE IF EXISTS CANDIDATE; ''')
     conn.commit()
     conn.execute(CREATE_USER_SQL)
     conn.execute(CREATE_STATUS_SQL)
     conn.execute(CREATE_VOTING_SQL)
-    conn.execute(CREATE_OPTION_SQL)
+    conn.execute(CREATE_CANDIDATURE_SQL)
+    conn.execute(CREATE_CANDIDATE_SQL)
     conn.commit()
     conn.close()
     logging.info("Base de datos creada correctamente.")
@@ -81,27 +93,30 @@ def reset_db(bot, message):
     conn.execute(''' DROP TABLE IF EXISTS USER; ''')
     conn.execute(''' DROP TABLE IF EXISTS STATUS; ''')
     conn.execute(''' DROP TABLE IF EXISTS VOTING; ''')
-    conn.execute(''' DROP TABLE IF EXISTS OPTION; ''')
+    conn.execute(''' DROP TABLE IF EXISTS CANDIDATURES; ''')
+    conn.execute(''' DROP TABLE IF EXISTS CANDIDATES; ''')
     conn.commit()
     bot.send_message(message.from_user.id, "Base de datos eliminada correctamente.")
     conn.execute(CREATE_USER_SQL)
     conn.execute(CREATE_STATUS_SQL)
     conn.execute(CREATE_VOTING_SQL)
-    conn.execute(CREATE_OPTION_SQL)
+    conn.execute(CREATE_CANDIDATURE_SQL)
+    conn.execute(CREATE_CANDIDATE_SQL)
     conn.commit()
     bot.send_message(message.from_user.id, "Base de datos creada correctamente.")
     conn.close()
 
 def set_is_login(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 1 WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 1, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
     conn.execute(''' UPDATE USER SET USERNAME = NULL, USER_ID = NULL, TOKEN = NULL WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' DELETE FROM VOTING WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
 def set_is_not_login(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0 WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
@@ -148,29 +163,42 @@ def get_save_token_and_id(id, base_url, password):
 
 
 def get_db():
-    return sqlite3.connect('sqlite.db')    
+    return sqlite3.connect('sqlite.db')
 
 def set_is_voting(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_VOTING = 'TRUE' WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 1, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' DELETE FROM VOTING WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
 def set_is_not_voting(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_VOTING = 'FALSE' WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
-def set_is_sending(id):
+def set_is_sending_1(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_SENDING = 'TRUE' WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 1, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
-def set_is_not_sending(id):
+def set_is_not_sending_1(id):
     conn = get_db()
-    conn.execute(''' UPDATE STATUS SET IS_SENDING = '0' WHERE CHAT_ID = %s; ''' % id)
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
+    conn.commit()
+    conn.close()
+
+def set_is_sending_2(id):
+    conn = get_db()
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 1 WHERE CHAT_ID = %s; ''' % id)
+    conn.commit()
+    conn.close()
+
+def set_is_not_sending_2(id):
+    conn = get_db()
+    conn.execute(''' UPDATE STATUS SET IS_LOGIN = 0, IS_VOTING = 0, IS_SENDING_1 = 0, IS_SENDING_2 = 0 WHERE CHAT_ID = %s; ''' % id)
     conn.commit()
     conn.close()
 
@@ -184,15 +212,51 @@ def create_voting(chat_id, vot_id , name , desc , p, g , y ):
     conn = get_db()
     conn.execute(''' INSERT INTO VOTING(CHAT_ID, VOT_ID , NAME , DESC , P, G , Y) VALUES (%s,%s,'%s','%s','%s','%s','%s'); ''' % (chat_id, vot_id , name , desc , p, g , y ))
     conn.commit()
-    conn.close()    
+    conn.close()
 
-def create_option(voting_id , number , text):
+def create_candidature(voting_id, number, text):
     conn = get_db()
-    conn.execute(''' INSERT INTO OPTION(VOTING_ID, NUMBER , TEXT) VALUES (%s,%s,'%s'); ''' % (voting_id, number , text ))
+    conn.execute(''' INSERT INTO CANDIDATURE(VOTING_ID, NUMBER, TEXT) VALUES (%s,%s,'%s'); ''' % (voting_id, number, text))
     conn.commit()
-    conn.close()    
+    conn.close()
 
+def get_candidatures(voting_id):
+    conn = get_db()
+    cursor = conn.execute(''' SELECT * FROM CANDIDATURE WHERE VOTING_ID = %s GROUP BY NUMBER; ''' % voting_id)
+    conn.commit()
+    v = cursor.fetchall()
+    conn.close()
+    return v
 
+def get_candidates(candidature_id):
+    conn = get_db()
+    cursor = conn.execute(''' SELECT * FROM CANDIDATE WHERE CANDIDATURE_ID = %s AND TYPE = 1 GROUP BY NUMBER; ''' % candidature_id)
+    conn.commit()
+    v = cursor.fetchall()
+    conn.close()
+    return v
+
+def get_candidate(number):
+    conn = get_db()
+    cursor = conn.execute(''' SELECT * FROM CANDIDATE WHERE NUMBER = %s AND TYPE = 1; ''' % number)
+    conn.commit()
+    v = cursor.fetchall()
+    conn.close()
+    return v
+
+def get_president(candidature_id):
+    conn = get_db()
+    cursor = conn.execute(''' SELECT * FROM CANDIDATE WHERE CANDIDATURE_ID = %s AND TYPE = 0; ''' % candidature_id)
+    conn.commit()
+    v = cursor.fetchall()[0]
+    conn.close()
+    return v
+
+def create_candidate(candidature_id, number, text, type):
+    conn = get_db()
+    conn.execute(''' INSERT INTO CANDIDATE(CANDIDATURE_ID, NUMBER, TEXT, TYPE) VALUES (%s,%s,'%s','%s'); ''' % (candidature_id, number, text, type))
+    conn.commit()
+    conn.close()
 
 def get_find_voting_and_get_options(chat_id, base_url, voting_id):
     res = False
@@ -200,31 +264,33 @@ def get_find_voting_and_get_options(chat_id, base_url, voting_id):
         "voting": voting_id,
     }
     response = requests.post(url=base_url + "/booth/getvoting/", data=form)
-    numbers =  []
-    options =  []
     if response.status_code is 200:
         res = True
+
         vot_id = int(response.json()['id'])
         name = response.json()['name']
         desc = response.json()['desc']
-        question = response.json()['question']
-        optionsJSONs = question['options']
-        for i in range(0, len(optionsJSONs)):
-            number = question['options'][i]['number']
-            option = question['options'][i]['option']
-            numbers.append(number)
-            options.append(option)
         pub_key = response.json()['pub_key']
         p = pub_key['p']
         g = pub_key['g']
         y = pub_key['y']
-        create_voting(chat_id, vot_id , name , desc , p, g , y )
+        create_voting(chat_id, vot_id, name, desc, p, g, y)
 
-        
+        for candidature in response.json()['candidatures']:
+            create_candidature(vot_id, candidature['id'], candidature['name'])
 
-    return res , numbers , options
+            for candidate in candidature['candidates']:
+                existe_presidente = False
+                type = 1
+                if candidate['type'] == 'PRESIDENCIA' and not existe_presidente:
+                    type = 0
+                    existe_presidente = True
+                create_candidate(candidature['id'], candidate['id'], candidate['name'], type)
+        return res, response.json()['candidatures']
+    else:
+        return res, None
 
- 
+
 def send_data(user, token, voting, vote, base_url):
     data = json.dumps({
             'voter': int(user),
--- bot.py
+++ bot.py
@@ -52,9 +52,13 @@ def login(message):
 @bot.message_handler(commands=['vote'])
 def vote_func(message):
     try:
+        if not auxiliar.check_value(message.from_user.id, "TOKEN", "USER"):
+            bot.send_message(message.from_user.id, "Por favor, primero inicia sesión para votar.")
+            login(message)
+            return
         auxiliar.set_is_voting(message.from_user.id)
-        bot.send_message(message.from_user.id, "Ahora, vas a transmitir un voto para una votación")
-        bot.send_message(message.from_user.id, "Por favor,indica el id de la votación en la que quieres participar")
+        bot.send_message(message.from_user.id, "Ahora vas a emitir un voto para una votación")
+        bot.send_message(message.from_user.id, "Por favor, indica el id de la votación en la que quieres participar")
     except Exception as e:
         logging.error("Ha ocurrido un problema al iniciar el proceso de login.", exc_info=True)        
 
@@ -68,7 +72,7 @@ def any_message(message):
             bot.send_message(chat_id, "¿Y tu contraseña?")
         except Exception as e:
             logging.error("Ha ocurrido un problema al guardar el username.", exc_info=True)
-    elif auxiliar.check_value(chat_id, "IS_LOGIN", "STATUS") and auxiliar.check_value(chat_id, "USERNAME", "USER") and not auxiliar.check_value(chat_id, "IS_VOTING", "STATUS"):
+    elif auxiliar.check_value(chat_id, "IS_LOGIN", "STATUS") and auxiliar.check_value(chat_id, "USERNAME", "USER"):
         try:
              if auxiliar.get_save_token_and_id(chat_id, BASE_URL, text):
                  auxiliar.set_is_not_login(chat_id)
@@ -80,65 +84,94 @@ def any_message(message):
         except Exception as e:
             logging.error("Ha ocurrido un problema al obtener las credenciales.", exc_info=True)
             
-    elif auxiliar.check_value(chat_id, "IS_VOTING", "STATUS") and not auxiliar.check_value(chat_id, "IS_SENDING", "STATUS") :
+    elif auxiliar.check_value(chat_id, "IS_VOTING", "STATUS"):
         try:
-             res , numbers , options  = auxiliar.get_find_voting_and_get_options(chat_id, BASE_URL, text)
-             if res:
-                 bot.send_message(message.from_user.id, "¡Se han encontrado coincidencias!")
-                 bot.send_message(message.from_user.id, "Ahora, selecciona la opción que quieras votar")
-                 markup = types.ReplyKeyboardMarkup(row_width=2 , one_time_keyboard= True)
-                 for i in range(0,len(options)):
-                     option = options[i]
-                     number  = numbers[i]
-                     optKeyboard = text + '.' +  str(number) + '#' + option
-                     itembtn = types.KeyboardButton(optKeyboard)
-                     markup.add(itembtn)  
-                     auxiliar.create_option(text , number , option)
-                 bot.send_message(chat_id, "Elige una opción:", reply_markup=markup)
-                 auxiliar.set_is_sending(chat_id)
-             else:
-                 bot.send_message(message.from_user.id, "Lo sentimos, no se ha encontrado ninguna votación con dicho Id.")
-                 bot.send_message(message.from_user.id, "Prueba de nuevo a enviar el id de la votación que quieres buscar.")
-                 auxiliar.set_is_not_sending(chat_id)
-                 vote_func(message)
-        except Exception as e:
-            logging.error("Ha ocurrido un problema al obtener tu voto.Vuelve a intentar votar", exc_info=True)
-
-    elif auxiliar.check_value(chat_id, "IS_VOTING", "STATUS") and auxiliar.check_value(chat_id, "IS_SENDING", "STATUS") :
-        try:
-            texto_recibido = text.split('.') #La opción llega con formato idVotacion.opcion # textoopcion 
-            vot_id = texto_recibido[0]
-            parteVoto= texto_recibido[1].split('#')
-            n_option = parteVoto[0]
-            texto_option = parteVoto[1].strip()
-            options = auxiliar.select_all_options_from_voting(vot_id)
-            if texto_option in options:
-                bot.send_message(message.from_user.id, "¡Perfecto!")
-                bot.send_message(message.from_user.id, "Ahora, procederemos a procesar tu voto....")
-                pup_key = auxiliar.make_pup_key(chat_id)
-                a, b = auxiliar.encrypt(pup_key,n_option)
-                user = str(auxiliar.select_param_user(chat_id ,'USER_ID' ))
-                token  =  str(auxiliar.select_param_user(chat_id ,'TOKEN' ))
-                vote =  [a, b]
-                final =  auxiliar.send_data(user, token, vot_id, vote, BASE_URL)
-                if final.status_code is 200:
-                    bot.send_message(message.from_user.id, "Su voto ha sido procesado, gracias por participar en la votación!")
-                else:
-                    bot.send_message(message.from_user.id, "Su voto no ha podido procesarse, por favor, vuelve a intentarlo")
-                    auxiliar.set_is_not_sending(chat_id)
-                    vote_func(message)
-              
+            res, candidatures = auxiliar.get_find_voting_and_get_options(chat_id, BASE_URL, text)
+            if res:
+                bot.send_message(message.from_user.id, "¡Se han encontrado coincidencias!")
+                bot.send_message(message.from_user.id, "Aquí tienes el listado de candidaturas y presidentes disponibles: ")
+                cont = 1
+                for candidature in candidatures:
+                    mensaje = str(cont) + ". " + candidature['name'].strip() + ": "
+                    existe_presidente = False
+                    for candidate in candidature['candidates']:
+                        if candidate['type'] == 'PRESIDENCIA' and not existe_presidente:
+                            existe_presidente = True
+                            mensaje = mensaje + candidate['name'].strip() + " (identificador: " + str(candidate['id']) + ")"
+                            bot.send_message(message.from_user.id, mensaje)
+                    cont = cont + 1
+                bot.send_message(message.from_user.id, "Por favor, para elegir uno de los presidentes, envía su identificador")
+                auxiliar.set_is_sending_1(chat_id)
             else:
-                bot.send_message(message.from_user.id, "No se ha detectado ninguna respuesta perteneciente a la votación.Por favor, repite el proceso otra vez.")
-                auxiliar.set_is_not_sending(chat_id)
+                bot.send_message(message.from_user.id, "Lo sentimos, no se ha encontrado ninguna votación con dicho Id.")
+                bot.send_message(message.from_user.id, "Prueba de nuevo a enviar el id de la votación que quieres buscar.")
+                auxiliar.set_is_voting(chat_id)
                 vote_func(message)
-                
+        except Exception as e:
+            logging.error("Ha ocurrido un problema al obtener tu voto. Vuelve a intentar votar", exc_info=True)
+    elif auxiliar.check_value(chat_id, "IS_SENDING_1", "STATUS"):
+        try:
+            presi = int(text.strip())
+            vot_id = auxiliar.check_value(chat_id, "VOT_ID", "VOTING")
+            candidatures_id = []
+            for candidature in auxiliar.get_candidatures(vot_id):
+                candidatures_id.append(candidature[0])
+            presidente_valido = False
+            for id in candidatures_id:
+                if auxiliar.get_president(id)[2] == presi and auxiliar.get_president(id)[4] == 0:
+                    presidente_valido = True
+                    break
+            if not presidente_valido:
+                bot.send_message(message.from_user.id, "Presidente incorrecto. Por favor, repite el proceso otra vez.")
+                auxiliar.set_is_voting(chat_id)
+                vote_func(message)
+                return
+            auxiliar.save_value(chat_id, presi, "PRESIDENTE", "STATUS")
+            candidatos = []
+            for cid in candidatures_id:
+                cand = auxiliar.get_candidates(cid)
+                for c in cand:
+                    candidatos.append(c)
+            bot.send_message(message.from_user.id, "¡Se han encontrado coincidencias!")
+            bot.send_message(message.from_user.id, "Aquí tienes el listado de candidatos disponibles: ")
+            cont = 1
+            for candidate in candidatos:
+                mensaje = str(cont) + ". " + candidate[3].strip() + " (identificador: " + str(candidate[2]) + ")"
+                bot.send_message(message.from_user.id, mensaje)
+                cont = cont + 1
+            bot.send_message(message.from_user.id, "Por favor, envía los identificadores de los candidatos elegidos separados por comas. Por ejemplo: '1,2,3'.")
+            auxiliar.set_is_sending_2(chat_id)
         except Exception as e:
             logging.error("Ha ocurrido un problema con el proceso. Por favor, vuelve a intentarlo", exc_info=True) 
             bot.send_message(message.from_user.id, "No se ha detectado ninguna respuesta perteneciente a la votación.Por favor, repite el proceso otra vez.")
-            auxiliar.set_is_not_sending(chat_id)
+            auxiliar.set_is_voting(chat_id)
             vote_func(message)
-         
+    elif not auxiliar.check_value(chat_id, "IS_VOTING", "STATUS") and not auxiliar.check_value(chat_id, "IS_SENDING_1", "STATUS") and auxiliar.check_value(chat_id, "IS_SENDING_2", "STATUS"):
+        voto_candidatos = text.strip().split(',')
+        for id in voto_candidatos:
+            if not auxiliar.get_candidate(id):
+                bot.send_message(message.from_user.id, "Candidatos incorrectos. Por favor, repite el proceso otra vez.")
+                auxiliar.set_is_voting(chat_id)
+                vote_func(message)
+                return
+        bot.send_message(message.from_user.id, "¡Perfecto!")
+        bot.send_message(message.from_user.id, "Ahora procederemos a procesar tu voto...")
+        pup_key = auxiliar.make_pup_key(chat_id)
+        voto_usuario = str(auxiliar.check_value(chat_id, "PRESIDENTE", "STATUS"))
+        for v in voto_candidatos:
+            voto_usuario = voto_usuario + "000" + str(v)
+        a, b = auxiliar.encrypt(pup_key, int(voto_usuario))
+        user = str(auxiliar.select_param_user(chat_id, 'USER_ID'))
+        token = str(auxiliar.select_param_user(chat_id, 'TOKEN'))
+        vote = [a, b]
+        final = auxiliar.send_data(user, token, auxiliar.check_value(chat_id, "VOT_ID", "VOTING"), vote, BASE_URL)
+        auxiliar.set_is_not_login(chat_id)
+        if final.status_code is 200:
+            bot.send_message(message.from_user.id, "Su voto ha sido procesado, ¡gracias por participar en la votación!")
+        else:
+            bot.send_message(message.from_user.id, "Su voto no ha podido procesarse, por favor, vuelve a intentarlo")
+            auxiliar.set_is_voting(chat_id)
+            vote_func(message)
             
 
 bot.polling(none_stop=True, timeout=120)
